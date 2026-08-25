03462e33897f3ec80c15a59ddf2ee9b7302c7bcd
abdiwaduddev <abdibasidhussein38@gmail.com>
2026-08-25 17:24:30 +0300

    Props

---
+++ React-Js/Exercise-3/src/App.jsx
@@ -0,0 +1,13 @@
+import UseCard from "./UseCard";
+
+const App = () => {
+  return (
+    <>
+      <UseCard name="Kalafoge" Email="Kalafoge@gmail.com" age="30" />
+      <UseCard name="Leo" Email="Leo@gmail.com" age="33" />
+      <UseCard name="Abdiwadud" Email="abdiwadud@gmail.com" age="39" />
+    </>
+  );
+};
+
+export default App;
---
+++ React-Js/Exercise-3/src/UseCard.jsx
@@ -0,0 +1,12 @@
+const UseCard = ({ name, Email, age }) => {
+  return (
+    <div>
+      <h1>Name : {name}</h1>
+      <p>Email : {Email}</p>
+      <p>Age: {age}</p>
+      <br />
+    </div>
+  );
+};
+
+export default UseCard;
